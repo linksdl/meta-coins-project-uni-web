f4dcacf172ee7289f0e349e5ff9960a10eec9254
Daolin Sheng <15900859362@163.com>
2022-09-06 23:55:25 +0800

    modified:   README.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # meta-coins-project-uni-web
+
+### mysql 启动
+- sudo chown -RL root:mysql /usr/local/mysql
+- sudo chown -RL mysql:mysql /usr/local/mysql/data
+- sudo /usr/local/mysql/support-files/mysql.server start ｜ stop
---
+++ src/test/java/com/meta/web/MetaCoinsProjectUniWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meta.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetaCoinsProjectUniWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
